5abc223829a6df7b3804e3564eacc1f8d34bd8d9
MiroslavAndric <mixxx123@gmail.com>
2023-05-07 23:06:31 +0200

    Adding mixins

--- src/app/components/home/header/header.component.ts
+++ src/app/components/home/header/header.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {  Component } from '@angular/core';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent  {
 
+  @HostListener('window:scroll', ['$event'])
+  
+  onWindowScroll() {
+      let element = document.querySelector('.header') as HTMLElement;
+      if (window.pageYOffset > element.clientHeight) {
+        element.classList.add('navbar-inverse');
+      } else {
+        element.classList.remove('navbar-inverse');
+      }
+    }
 }
